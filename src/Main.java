9c4344a6366fb42eb2db98d3cce87a66294e2e78
nourabyte <148945652+nourabyte@users.noreply.github.com>
2023-11-30 13:58:33 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer[] numbers = {1, 2, 3, 4, 5};
+        ArrayUtils.printArray(numbers);
+
+        String[] names = {"John", "Jane", "Alice"};
+        ArrayUtils.printArray(names);
+    }
+}
